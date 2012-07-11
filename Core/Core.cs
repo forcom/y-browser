f91adc3b62e324b42084d8a96f577fd4bf1d0fcc
Yoon SeungYong <forcom.kr@gmail.com>
2012-07-12 03:15:33 +0900

    Created the Data Structures of HTML

--- Core/Core.cs
+++ Core/Core.cs
@@ -5,4 +5,165 @@ using System.Text;
 
 namespace Core
 {
+    /// <summary>
+    /// Represents an html attribute.
+    /// </summary>
+    public class Attribute
+    {
+        /// <summary>
+        /// Name of the attribute.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Value of the attribute.
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// An attribute
+        /// </summary>
+        /// <param name="name">Attribute name</param>
+        /// <param name="value">Attribute value</param>
+        public Attribute(string name, string value = "")
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+
+    /// <summary>
+    /// Represents a collection of attributes.
+    /// </summary>
+    public class AttributeCollection
+    {
+        Dictionary<string, Attribute> _Items = new Dictionary<string, Attribute>(StringComparer.OrdinalIgnoreCase);
+        /// <summary>
+        /// Get or set of attributes in the collection
+        /// </summary>
+        /// <param name="key">A attribute name</param>
+        /// <returns>An attribute</returns>
+        public Attribute this[string key]
+        {
+            get
+            {
+                return _Items[key];
+            }
+            set
+            {
+                _Items[key] = value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Represents an html element.
+    /// </summary>
+    public class Element
+    {
+        /// <summary>
+        /// Represents a type of element.
+        /// </summary>
+        public enum ElementType
+        {
+            Special,
+            Structure,
+            Markup
+        }
+
+        /// <summary>
+        /// Name of the element
+        /// </summary>
+        public string Name { get; set; }
+        AttributeCollection _Attributes = new AttributeCollection();
+        /// <summary>
+        /// Attributes in the element
+        /// </summary>
+        public AttributeCollection Attributes { get { return _Attributes; } }
+        /// <summary>
+        /// Check the element is a start tag.
+        /// </summary>
+        public bool IsStartTag { get; set; }
+        /// <summary>
+        /// Type of the element
+        /// </summary>
+        public ElementType Type { get; set; }
+
+        /// <summary>
+        /// Get or set an attribute in the element
+        /// </summary>
+        /// <param name="attribute">Attribute Name</param>
+        /// <returns>An attribute</returns>
+        public Attribute this[string attribute]
+        {
+            get
+            {
+                return Attributes[attribute];
+            }
+            set
+            {
+                Attributes[attribute] = value;
+            }
+        }
+
+        /// <summary>
+        /// An element
+        /// </summary>
+        /// <param name="name">Element name</param>
+        /// <param name="starttag">Tag type</param>
+        public Element(string name, bool starttag = true)
+        {
+            Name = name;
+            IsStartTag = starttag;
+        }
+    }
+
+    /// <summary>
+    /// Represents a collection of elements
+    /// </summary>
+    public class ElementCollection
+    {
+        List<Element> _Items = new List<Element>();
+        /// <summary>
+        /// A collection of elements.
+        /// </summary>
+        public List<Element> Items { get { return _Items; } }
+
+        /// <summary>
+        /// Elements with the name
+        /// </summary>
+        /// <param name="name">Name of the element</param>
+        /// <returns>Elements with the name</returns>
+        public Element[] this[string name]
+        {
+            get
+            {
+                return Items.FindAll(x => x.Attributes["name"] != null ? x.Attributes["name"].Name == name : false).ToArray();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Represents a html document.
+    /// </summary>
+    public class Document
+    {
+        ElementCollection _Items = new ElementCollection();
+        /// <summary>
+        /// A collection of elements
+        /// </summary>
+        public ElementCollection Items { get { return _Items; } }
+
+        /// <summary>
+        /// A kind of elements
+        /// </summary>
+        /// <param name="element">Element type</param>
+        /// <returns>Elements</returns>
+        public Element[] this[string element]
+        {
+            get
+            {
+                return Items.Items.FindAll(x => x.Name == element).ToArray();
+            }
+        }
+    }
 }
