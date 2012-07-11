e300d63e7dceef8caa5b3fb03171f7d9b79ca86f
Yoon SeungYong <forcom.kr@gmail.com>
2012-07-12 03:21:17 +0900

    Test Project Created.

---
+++ Test/TestCore.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    /// <summary>
+    /// TestCore の概要の説明
+    /// </summary>
+    [TestClass]
+    public class TestCore
+    {
+        public TestCore()
+        {
+            //
+            // TODO: コンストラクター ロジックをここに追加します
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///現在のテストの実行についての情報および機能を
+        ///提供するテスト コンテキストを取得または設定します。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 追加のテスト属性
+        //
+        // テストを作成する際には、次の追加属性を使用できます:
+        //
+        // クラス内で最初のテストを実行する前に、ClassInitialize を使用してコードを実行してください
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // クラス内のテストをすべて実行したら、ClassCleanup を使用してコードを実行してください
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // 各テストを実行する前に、TestInitialize を使用してコードを実行してください
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // 各テストを実行した後に、TestCleanup を使用してコードを実行してください
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestCore()
+        {
+        }
+    }
+}
