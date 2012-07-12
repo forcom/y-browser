a2f0317c20843719815b48a4260832212aa079c6
Yoon SeungYong <forcom.kr@gmail.com>
2012-07-13 02:34:35 +0900

    HtmlReader Created.

---
+++ Core/HtmlReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Core
+{
+    public class HtmlReader
+    {
+        public static Document GetDocument(string html)
+        {
+            Document doc = new Document();
+            return doc;
+        }
+    }
+}
