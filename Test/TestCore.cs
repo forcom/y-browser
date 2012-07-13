e15a77c55449c10a1c6b210b688f9aeef13605f2
Yoon SeungYong <forcom.kr@gmail.com>
2012-07-14 04:31:00 +0900

    Add TestCases for HtmlReader

--- Test/TestCore.cs
+++ Test/TestCore.cs
@@ -60,73 +60,6 @@ namespace Test
         //
         #endregion
 
-        string MakeHtml(Core.Document doc)
-        {
-            StringBuilder sb = new StringBuilder();
-            foreach (var i in doc.Items)
-            {
-                switch (i.Type)
-                {
-                    case Element.ElementType.Special:
-                    case Element.ElementType.Object:
-                        sb.Append('<');
-                        if (!i.IsStartTag) sb.Append('/');
-                        sb.Append(i.Name);
-                        foreach (var j in i.Attributes.Items.Values)
-                        {
-                            sb.Append(' ');
-                            sb.Append(j.Name);
-                            if (j.Value != "")
-                            {
-                                sb.Append("=\"");
-                                sb.Append(j.Value);
-                                sb.Append("\"");
-                            }
-                        }
-                        sb.Append(">\n");
-                        break;
-                    case Element.ElementType.Text:
-                        sb.Append(i.Name);
-                        break;
-                    case Element.ElementType.Structure:
-                        sb.Append("\n<");
-                        if (!i.IsStartTag) sb.Append('/');
-                        sb.Append(i.Name);
-                        foreach (var j in i.Attributes.Items.Values)
-                        {
-                            sb.Append(' ');
-                            sb.Append(j.Name);
-                            if (j.Value != "")
-                            {
-                                sb.Append("=\"");
-                                sb.Append(j.Value);
-                                sb.Append("\"");
-                            }
-                        }
-                        sb.Append(">\n");
-                        break;
-                    case Element.ElementType.Markup:
-                        sb.Append("<");
-                        if (!i.IsStartTag) sb.Append('/');
-                        sb.Append(i.Name);
-                        foreach (var j in i.Attributes.Items.Values)
-                        {
-                            sb.Append(' ');
-                            sb.Append(j.Name);
-                            if (j.Value != "")
-                            {
-                                sb.Append("=\"");
-                                sb.Append(j.Value);
-                                sb.Append("\"");
-                            }
-                        }
-                        sb.Append(">");
-                        break;
-                }
-            }
-            return sb.ToString();
-        }
-
         [TestMethod]
         public void TestHtmlReader()
         {
@@ -159,6 +92,168 @@ end tag has been omitted.
 <IMG SRC =""triangle.xbm"" alt=""Warning: "">
 Be sure to read these <b>bold instructions</b>.
 </BODY></HTML>";
+            string html2 = @"<!DOCTYPE HTML PUBLIC ""-//IETF//DTD HTML 2.0 Strict//EN"">
+<HTML>
+<HEAD>
+  <TITLE>HTML Working Group of the IETF (at W3C)</TITLE>
+</HEAD>
+<BODY>
+<P>
+<A HREF=""../../""><IMG BORDER=""0"" ALT=""W3C"" SRC=""../../Icons/WWW/w3c_home""
+    ALT=""W3C""></A> | <A HREF=""../"">HTML</A>
+<H1>
+  <A HREF=""http://www.ietf.org/""><IMG BORDER=""0"" SRC=""../../Icons/WWW/ietf_48x84.gif""
+      ALT=""IETF"" WIDTH=""84"" HEIGHT=""48""></A> HTML WG
+</H1>
+<H2>
+  NOTE: This working group is closed
+</H2>
+<PRE>
+Date: Thu, 12 Sep 1996 08:52:30 -0500
+Message-Id: &lt;2.2.32.19960912135230.00b6c3f8@spyglass.com&gt;
+To: html-wg@w3.org
+From: ""Eric W. Sink"" <eric@spyglass.com>
+Subject: This WG is now closed
+</PRE>
+<P>
+See: <A href=""../Activity"">W3C HTML Activity</A> for HTML development status.
+<BLOCKQUOTE>
+  The HTML Working Group is chartered firstly to describe, and secondly to
+  develop, the HyperText Markup Language (HTML). The group's work is to be
+  based on existing practice on the Internet, and will make due reference to
+  the SGML standard.
+  <ADDRESS>
+    from the
+    <A HREF=""http://www.ietf.cnri.reston.va.us/proceedings/94dec/charters/html-charter.html"">charter
+    of the HTML WG</A><BR>
+    as of IETF 31, Dec 1994
+  </ADDRESS>
+</BLOCKQUOTE>
+<P>
+If you are new to the <A href=""http://www.ietf.org/"">IETF</A>, you should
+probably do some background reading. I recommend:
+<UL>
+  <LI>
+    <A href=""ftp://ds.internic.net/ietf/1wg-guidelines.txt"">IETF Working Group
+    Process</A>
+  <LI>
+    <A href=""ftp://ds.internic.net/ietf/1id-guidelines.txt"">Guidelines to Draft
+    Authors</A>
+  <LI>
+    <A href=""http://www.ietf.cnri.reston.va.us/tao.html"">The TAO of the IETF</A>
+</UL>
+<P>
+IETF working groups exist for the sole purpose of drafting, revising, &nbsp;and
+reviewing Internet Standards. They have a focused charter and milestones.
+In order to conduct the business of the working group effectively, discussion
+of items not on the charter is prohibited (this rule is enforced by the working
+group chair, and sometimes the members. <STRONG>You have been warned</STRONG>.)
+There are a&nbsp;number of <A HREF=""../#discussion"">other HTML and WWW discussion
+forums</A>, such as mailing lists and USENET newsgroups, where discussions
+of&nbsp;philosophy, proposed features, and announcements of WWW systems and
+resources are welcome.
+<H2>
+  Mailing List
+</H2>
+<P>
+The business of the working group is conducted on a&nbsp;public mailing list,
+<TT>&lt;html-wg@w3.org&gt;</TT> (formerly html-wg@oclc.org).
+<H2>
+  Background and Archives
+</H2>
+<DL>
+  <DT>
+    <A HREF=""http://www.ietf.cnri.reston.va.us/html.charters/html-charter.html"">HTML
+    working group charter</A>
+  <DD>
+    maintained by the IETF secretariate
+    <TT>&lt;ietf-web@cnri.reston.va.us&gt;</TT>
+  <DT>
+    <A HREF=""http://listserv.heanet.ie/html-wg.html""><!--
+    <A HREF=""http://www.acl.lanl.gov/HTML_WG/archives.html"">-->Hypertext Archive
+    of HTML-WG mailing list</A>
+  <DD>
+    formerly maintained by Ron Daniel<BR>
+    <!--no longer there, but there are
+    <A HREF=""http://www.altavista.com/cgi-bin/query?pg=q&amp;kl=XX&amp;q=link%3Ahttp%3A%2F%2Fwww.acl.lanl.gov%2FHTML_WG%2F"">lots
+    of links to it</A>-->
+  <DT>
+    <A HREF=""http://www.ics.uci.edu/pub/ietf/html/"">HTML-WG: Abstracts and Related
+    Info</A>
+  <DD>
+    maintained by Roy Fielding
+  <DT>
+    <A HREF=""http://www.acl.lanl.gov/HTML/html-archive.subject-index.html"">HTML
+    Implementors Group mailing list archive</A>
+  <DD>
+    Archive of messages from June 14, 1994 to October 14, 1994, dealing with
+    revisions to the HTML 2.0 specifications.
+    (<A HREF=""http://www.devry-phx.edu/webresrc/webmstry/langtech.htm"">source</A>)
+</DL>
+<H2>
+  Principals
+</H2>
+<DL>
+  <DT>
+    HTML Working Group Chair
+  <DD>
+    Eric Sink <TT>esink@spyglass.com</TT>
+  <DT>
+    Editor of the HTML 2.n Specification:
+  <DD>
+    Dan Connolly <TT>connolly@w3.org</TT>
+  <DT>
+    Editor of the 3.n Specification(s)
+  <DD>
+    Dave Raggett <TT>dsr@w3.org</TT>
+</DL>
+<H2>
+  Proposing New Features
+</H2>
+<P>
+Those who would propose new features or modify old ones are urged to discuss
+their ideas with experienced HTML implementors before going public. In any
+case, the following issues should be addressed in any proposal:
+<UL>
+  <LI>
+    a statement of the problem as you see it
+  <LI>
+    a proposed solution
+  <LI>
+    a demonstration that this solution is globally cost-effective without being
+    locally prohibitive (i.e. the sum of all the effort of deploying this solution
+    is less than the cost of dealing with the problem with existing technology,
+    and yet no one party bears too much of the burden. For example, if you require
+    every information provider to do something, it had better be minimal.)
+  <LI>
+    a discussion of graceful deployment and interoperability issues.
+</UL>
+<H3>
+  <STRONG>**** NOTE WELL **** </STRONG>
+</H3>
+<P>
+Your ideas will achieve greater credence if you take the time and effort
+to disseminate proposals as an internet draft. Don't forget to follow the
+internet draft guidelines:
+<A HREF=""ftp://ietf.cnri.reston.va.us/internet-drafts/1id-guidelines.txt""><CITE>Guidelines
+to Authors of Internet-Drafts</CITE></A>
+<BLOCKQUOTE>
+  ""We value your opinion, really we do, but we're going to put a stiff tax
+  on its expression, so that we won't have to hear it very often.""
+  <ADDRESS>
+    -name withheld to protect the guilty ;-)
+  </ADDRESS>
+</BLOCKQUOTE>
+<P>
+<P>
+  <HR>
+<ADDRESS>
+  <A HREF=""../../People/Connolly/"">Dan Connolly</A><BR>
+  <SMALL>$Revision: 1.11 $ $Author: dsr $<BR>
+  $Date: 2000/01/31 19:30:15 $</SMALL>
+</ADDRESS>
+</BODY></HTML>
+";
             HtmlReader.GetDocument(html);
         }
 
@@ -294,7 +389,9 @@ Be sure to read these <b>bold instructions</b>.
             doc.Items.Add(body_e);
             doc.Items.Add(html_e);
 
-            string Result = MakeHtml(doc);
+            string Result = Function.MakeHtml(doc);
+
+            Assert.AreEqual(true, true);
         }
     }
 }
