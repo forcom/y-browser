dfd6898d8f51cf35e24134d7c5af9e7f54f44829
Yoon SeungYong <forcom.kr@gmail.com>
2012-07-16 05:36:01 +0900

    Add Get Page

--- YWebView/YWebView.Designer.cs
+++ YWebView/YWebView.Designer.cs
@@ -28,14 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.Page = new System.Windows.Forms.RichTextBox();
+            this.tmrNavigate = new System.Windows.Forms.Timer(this.components);
+            this.tmrShowPage = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Page
+            // 
+            this.Page.BackColor = System.Drawing.Color.White;
+            this.Page.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Page.DetectUrls = false;
+            this.Page.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Page.ForeColor = System.Drawing.Color.Black;
+            this.Page.Location = new System.Drawing.Point(0, 0);
+            this.Page.Name = "Page";
+            this.Page.ReadOnly = true;
+            this.Page.Size = new System.Drawing.Size(800, 600);
+            this.Page.TabIndex = 0;
+            this.Page.Text = "";
+            // 
+            // tmrShowPage
+            // 
+            this.tmrShowPage.Tick += new System.EventHandler(this.tmrShowPage_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(443, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // YWebView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Page);
             this.ForeColor = System.Drawing.Color.Black;
             this.Name = "YWebView";
             this.Size = new System.Drawing.Size(800, 600);
@@ -44,5 +78,12 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox Page;
+        private System.Windows.Forms.Timer tmrNavigate;
+        private System.Windows.Forms.Timer tmrShowPage;
+        private System.Windows.Forms.Button button1;
+
+
     }
 }
--- YWebView/YWebView.cs
+++ YWebView/YWebView.cs
@@ -6,14 +6,51 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.IO;
+using Core;
 
 namespace YWebView
 {
     public partial class YWebView : UserControl
     {
+        public string Title { get; set; }
+
+        NetProcess np = new NetProcess();
+        Document curDoc = null;
+
         public YWebView()
         {
             InitializeComponent();
+            Title = "";
+            tmrNavigate.Tick += new EventHandler(np.tmrNavigate_Tick);
+        }
+
+        public void Navigate(string Url)
+        {
+            np.Navigate(Url);
+            tmrNavigate.Enabled = true;
+            tmrShowPage.Enabled = true;
+        }
+
+        void ShowPage()
+        {
+            curDoc = np.CurrentPage;
+        }
+
+        private void tmrShowPage_Tick(object sender, EventArgs e)
+        {
+            if (tmrNavigate.Enabled) return;
+            /*Thread thr = new Thread(new ThreadStart(ShowPage));
+            thr.Start();*/
+            tmrShowPage.Enabled = false;
+            ShowPage();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Navigate("http://www.w3.org/MarkUp/html-spec/");
         }
     }
 }
